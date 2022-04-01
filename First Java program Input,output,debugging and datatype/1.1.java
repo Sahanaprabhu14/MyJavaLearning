624c673007de6a3dc463324969301953068f2158
Sahana <sahanaprabhu224@gmail.com>
2022-04-01 14:49:46 +0530

    Added program 1.1

---
+++ First Java program Input,output,debugging and datatype/1.1.java
@@ -0,0 +1,13 @@
+
+//Scanner is a class that allows us to take input and located in java.util package
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        //standard output stream
+        System.out.println("Hello World!");
+        //standard input stream (keyboard input)
+        Scanner input = new Scanner(System.in);
+
+    }
+}
