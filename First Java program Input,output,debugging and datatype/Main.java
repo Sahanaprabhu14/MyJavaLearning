a6aa10d3e42850470803758d292c7d7d41a9ec16
Sahana <sahanaprabhu224@gmail.com>
2022-04-01 15:09:15 +0530

    Modified files

--- First Java program Input,output,debugging and datatype/Main.java
+++ First Java program Input,output,debugging and datatype/Main.java
@@ -2,12 +2,14 @@
 //Scanner is a class that allows us to take input and located in java.util package
 import java.util.Scanner;
 
-public class Main{
+public class Main {
     public static void main(String[] args) {
         //standard output stream
         System.out.println("Hello World!");
         //standard input stream (keyboard input)
         Scanner input = new Scanner(System.in);
+        //Prints the next integer Note: You can only pass integer
+        System.out.println(input.nextInt());
 
     }
 }
